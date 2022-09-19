cb0ee2f0f34737731529f1589bf3cb753a43ac3a
marcmeynet <info@marcmeynet.ch>
2022-09-19 03:46:49 +0200

    install chakraui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ColorCircle from './components/ColorCircle';
 
 function App() {
   return (
     <div className="app">
       <h1 className="app-title">Color Tones</h1>
+      <ColorCircle />
     </div>
   );
 }
---
+++ src/components/ColorCircle.js
@@ -0,0 +1,5 @@
+const ColorCircle = () => {
+    return <div></div>;
+};
+
+export default ColorCircle;
